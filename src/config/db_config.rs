fe3b37195ed7051d7cfecd1fdf50b8a03d925020
levish0 <shiueo.csh@gmail.com>
2025-09-30 19:27:22 +0900

    Update db_config.rs

--- src/config/db_config.rs
+++ src/config/db_config.rs
@@ -6,11 +6,14 @@ use std::sync::LazyLock;
 pub struct DbConfig {
     pub is_dev: bool,
 
+    pub wiki_server_host: String,
+    pub wiki_server_port: String,
+
     pub server_host: String,
     pub server_port: String,
 }
 
-// LazyLock으로 자동 초기화
+// LazyLock
 static CONFIG: LazyLock<DbConfig> = LazyLock::new(|| {
     dotenv().ok();
 
@@ -21,6 +24,9 @@ static CONFIG: LazyLock<DbConfig> = LazyLock::new(|| {
 
     DbConfig {
         is_dev,
+
+        wiki_server_host: env::var("WIKI_SERVER_HOST").unwrap_or_else(|_| "127.0.0.1".to_string()),
+        wiki_server_port: env::var("WIKI_SERVER_PORT").unwrap_or_else(|_| "8000".to_string()),
 
         server_host: env::var("HOST").expect("HOST must be set in .env file"),
         server_port: env::var("PORT").expect("PORT must be set in .env file"),
