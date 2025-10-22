3a3220fe29809a44d4ce567e03a4a9e2c83c5c50
levish0 <shiueo.csh@gmail.com>
2025-10-22 18:12:14 +0900

    Update routes.rs

--- src/api/v0/routes/parse/routes.rs
+++ src/api/v0/routes/parse/routes.rs
@@ -3,6 +3,6 @@ use crate::state::AppState;
 use axum::Router;
 use axum::routing::post;
 
-pub fn parse_routes(state: AppState) -> Router<AppState> {
+pub fn parse_routes(_state: AppState) -> Router<AppState> {
     Router::new().route("/parse", post(parse_endpoint))
 }
