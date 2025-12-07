22f78d14898bdd515d085a5196399aa73598b7ef
levish0 <shiueo.csh@gmail.com>
2025-12-07 18:32:07 +0900

    dispatch

--- sevenmark-parser/src/parser/element.rs
+++ sevenmark-parser/src/parser/element.rs
@@ -22,25 +22,20 @@ use crate::parser::r#macro::{
     macro_null_parser, macro_variable_parser,
 };
 use winnow::Result;
-use winnow::combinator::alt;
-use winnow::combinator::repeat;
+use winnow::combinator::{alt, dispatch, peek, repeat};
 use winnow::prelude::*;
+use winnow::token::any;
 
 pub fn element_parser(parser_input: &mut ParserInput) -> Result<Vec<SevenMarkElement>> {
     repeat(
         1..,
-        alt((
-            // Escape
-            escape_parser,
-            // Comment
-            multiline_comment_parser,
-            inline_comment_parser,
-            // Brace
-            alt((
+        dispatch! {peek(any);
+            '\\' => alt((escape_parser, token_backslash_parser)),
+            '/' => alt((multiline_comment_parser, inline_comment_parser, token_slash)),
+            '{' => alt((
                 brace_include_parser,
                 brace_category_parser,
                 brace_if_parser,
-                // brace w/parameters
                 brace_table_parser,
                 brace_list_parser,
                 brace_fold_parser,
@@ -52,43 +47,30 @@ pub fn element_parser(parser_input: &mut ParserInput) -> Result<Vec<SevenMarkEle
                 brace_define_parser,
                 brace_style_parser,
                 brace_literal_parser,
+                token_brace_open_parser,
             )),
-            // Bracket
-            bracket_media_parser,
-            alt((
-                markdown_header_parser,
-                markdown_hline_parser,
-                markdown_bold_parser,
-                markdown_italic_parser,
-                markdown_underline_parser,
-                markdown_strikethrough_parser,
-                markdown_superscript_parser,
-                markdown_subscript_parser,
-            )),
-            alt((
+            '}' => token_brace_close_parser,
+            '[' => alt((
+                bracket_media_parser,
                 macro_now_parser,
                 macro_newline_parser,
                 macro_variable_parser,
                 macro_age_parser,
                 macro_footnote_parser,
                 macro_null_parser,
+                token_bracket_open_parser,
             )),
-            // Text,
-            text_parser,
-            // Token,
-            token_newline_parser,
-            token_brace_open_parser,
-            token_brace_close_parser,
-            token_bracket_open_parser,
-            token_bracket_close_parser,
-            token_slash,
-            token_asterisk_parser,
-            token_underscore_parser,
-            token_tilde_parser,
-            token_caret_parser,
-            token_comma_parser,
-            token_backslash_parser,
-        )),
+            ']' => token_bracket_close_parser,
+            '#' => alt((markdown_header_parser, text_parser)),
+            '-' => alt((markdown_hline_parser, text_parser)),
+            '*' => alt((markdown_bold_parser, markdown_italic_parser, token_asterisk_parser)),
+            '_' => alt((markdown_underline_parser, token_underscore_parser)),
+            '~' => alt((markdown_strikethrough_parser, token_tilde_parser)),
+            '^' => alt((markdown_superscript_parser, token_caret_parser)),
+            ',' => alt((markdown_subscript_parser, token_comma_parser)),
+            '\n' => token_newline_parser,
+            _ => text_parser,
+        },
     )
     .parse_next(parser_input)
 }
