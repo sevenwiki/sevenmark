4472ae61f067642809dc6ad9c2797daf418e17df
levish0 <shiueo.csh@gmail.com>
2026-02-19 19:37:53 +0900

    misc

--- editors/jetbrains/bin/main/com/sevenwiki/sevenmark/SevenMarkIcons.kt
+++
@@ -1,8 +0,0 @@
-package com.sevenwiki.sevenmark
-
-import com.intellij.openapi.util.IconLoader
-
-object SevenMarkIcons {
-    @JvmField
-    val FILE = IconLoader.getIcon("/icons/sevenmark.svg", SevenMarkIcons::class.java)
-}
