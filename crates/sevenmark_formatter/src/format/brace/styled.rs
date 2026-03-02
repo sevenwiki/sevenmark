224973c4603f6382c67d9c3abce2fcbe69d45749
levish0 <shiueo.csh@gmail.com>
2026-03-02 13:18:14 +0900

    fmt

--- crates/sevenmark_formatter/src/format/brace/styled.rs
+++ crates/sevenmark_formatter/src/format/brace/styled.rs
@@ -2,9 +2,9 @@ use pretty::{Arena, DocAllocator, DocBuilder};
 use sevenmark_ast::StyledElement;
 
 use crate::FormatConfig;
+use crate::format::brace::raw::needs_line_break_before_brace_close;
 use crate::format::element::format_elements;
 use crate::format::params::format_params_block;
-use crate::format::brace::raw::needs_line_break_before_brace_close;
 
 pub fn format_styled<'a>(
     a: &'a Arena<'a>,
